5193865557b98d991e3c6cecb2a3b8fe36825314
markoduarte <marco.duarte.etech@gmail.com>
2025-04-22 20:55:24 -0400

    comintando a correção

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World from PaaS!'));
+app.get('/', (req, res) => res.send('Hello World do PaaS!'));
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
